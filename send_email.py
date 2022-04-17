846322391996f660710e8bd4b2d36b7207193d13
SRCinVA <scitarella@hotmail.com>
2022-04-17 16:24:58 -0400

    more progress

--- send_email.py
+++ send_email.py
@@ -7,3 +7,14 @@ body = "This is a test email from Python!"
 sender_email = "srcgithub@gmail.com"
 receiver_email = "srcgithub@gmail.com"
 password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)  # notice the built-in method
+
+context = ssl.create_default_context()  # this gives us a secure connection to Gmail's server, since we dont have our own server
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: # 465 is the port
+    pass
