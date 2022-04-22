36decfed467978fcc583cd770a5b4c79b280ebf8
SRCinVA <scitarella@hotmail.com>
2022-04-21 20:37:16 -0400

    Entered some html

--- send_email.py
+++ send_email.py
@@ -14,12 +14,21 @@ message["To"] = receiver_email
 message["Subject"] = subject
 message.set_content(body)  # notice the built-in method
 
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
 context = ssl.create_default_context()  # this gives us a secure connection to Gmail's server, since we dont have our own server
 
 print("Sending Email")
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: # 465 is the port
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message.as_string)
+    server.sendmail(sender_email, receiver_email, message.as_string())
 
 print("Success!")
